ed5ea24e8c5b43f9324da5dfe70938dab9b65785
Notanum Bit <notanumbit@none.com>
2019-12-12 04:04:39 +0100

    Remove listen function

--- bot.js
+++ bot.js
@@ -1,12 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
-
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
